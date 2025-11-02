12671b8c1097b09e51e907e8561d89527e6a9118
AndrewKostiv <AndrewKostiv1@gmail.com>
2025-11-02 00:55:30 -0400

    first commit

---
+++ react-basics/src/Greeting.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Greeting() {
+  const name = "Andrew";
+  return (
+    <>
+      <h2>Hello, {name} 👋</h2>
+      <p>Welcome to React!</p>
+    </>
+  );
+}
+
+export default Greeting;
